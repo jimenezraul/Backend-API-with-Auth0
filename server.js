b726638be1739381d4cc5efa10c1795802c9903f
Raul <jimenezraul1981@gmail.com>
2024-03-19 16:43:45 -0400

    Update auth middleware

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const auth = require('./auth');
+const auth = require('./authMiddleware');
 const cors = require('cors');
 
 const port = process.env.PORT || 8080;
